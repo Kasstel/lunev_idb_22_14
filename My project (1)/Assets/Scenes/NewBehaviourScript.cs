f95841f80db8c574fa7d1c1bfb1c5a809e45d671
Kasstel <kasstel03@mail.ru>
2023-10-02 23:42:21 +0300

    Homework_first

---
+++ My project (1)/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Uptade");
+    }
+}
