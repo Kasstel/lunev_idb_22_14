11aca67ee001709c03c24f6345f440e845486d2c
Kasstel <kasstel03@mail.ru>
2023-10-17 15:39:08 +0300

    Homework2_Collision+Delete(HW1)

--- Lunev_IDB_22_14/Assets/Scripts/cube.cs
+++ Lunev_IDB_22_14/Assets/Scripts/cube.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class cube : MonoBehaviour
 {
+    public int c;
+
+    public float Speed = 10f;
+  
     public GameObject CUBE;
 
+    public GameObject Sphere;
 
+    public Text x;
 
-
+    public GameObject Cam;
+    public GameObject BasicCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +25,30 @@ public class cube : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * 5*Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * 5 * Speed * Time.deltaTime);
+        }
+        if (c <= 0)
+        {
+            Cam.SetActive(true);
+            BasicCam.SetActive(false);
+            this.gameObject.SetActive(false);
+            x.text = "Игра Окончена";
+        }
+        else x.text = c.ToString();
     }
 
     public void Scene()
@@ -36,4 +66,32 @@ public class cube : MonoBehaviour
             
     }
 
+    private void OnCollisionEnter( Collision collision)
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        c--;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void heal()
+    {
+        c = 6;
+    }
+    /*private void OnMouseDown()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    private void OnMouseUp() {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }                            
+
+    private void OnMouseEnter()
+    {
+        Sphere.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }*/
 }
